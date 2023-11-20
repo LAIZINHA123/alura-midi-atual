bf5b1496ed09bfabbe7dc649fe306affc762be0e
LAIZINHA123 <130383296+LAIZINHA123@users.noreply.github.com>
2023-11-20 08:02:05 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -4,25 +4,18 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-
-    
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom');
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
 
-    contador = contador + 1;
-
-    console.log(contador);
-
 }
-
-
-   
-
 
 
 
